9892ccbcbd3eb9e3afc81d006b182b41ad37ddee
Mikhail Miroshnikov <106276818+mirrosha26@users.noreply.github.com>
2023-01-10 09:04:30 +0000

    Change H30

--- H30.py
+++ H30.py
@@ -1,8 +1,12 @@
-# задаем список
+# Правки: нужно поменть на функцию
+def  min_reduction(mas):
+    min_len = len(mas[0])
+    for i in range(1, len(mas)):
+        check_len = len(mas[i])
+        if check_len < min_len:
+            min_len= check_len
+    return list(map(lambda x:x[:min_len] , mas ))
+
+# тест
 mas = ['Привет', 'Пока', 'И ты?', 'И я', 'О, да']
-min_len = len(mas[0])
-for i in range(1, len(mas)):
-    check_len = len(mas[i])
-    if check_len < min_len:
-        min_len= check_len
-print(list(map(lambda x:x[:min_len] , mas )))a
+print(min_reduction(mas))
